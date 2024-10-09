c4d49c0037be03b1cd4151c64e924f744b60ab87
jomm <joemired@gmail.com>
2024-10-10 02:21:46 +0800

    asd

--- PaidG/src/main/java/com/example/paidg/HelloApplication.java
+++ PaidG/src/main/java/com/example/paidg/HelloApplication.java
@@ -1,10 +1,8 @@
 package com.example.paidg;
 
-import com.sun.tools.javac.Main;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
